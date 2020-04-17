1e2f6f16a9f6fbd0425e98fc3edba6d12aab8d85
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-04-17 10:22:02 -0500

    use effect done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,19 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const App = () => {
+    const [starWars, setStarWars] = useState([])
+
+    useEffect(() => {
+      axios.get("https://swapi.py4e.com/api/people/")
+      
+      .then(res => setStarWars(res.data.results))
+      
+      .catch(err => console.log(err, "error"))
+
+    })
+  }
 
   return (
     <div className="App">
