8ef56bb33d355dc26a37b891bb6be781bf182f35
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-04-17 10:42:36 -0500

    set up to start crush character page

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import Character from './components/Character'
 import './App.css';
+import styled from 'styled-components'
 
-const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,22 +12,43 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const App = () => {
-    const [starWars, setStarWars] = useState([])
+  const MainContainer = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
+  margin:2%;
+`;
 
-    useEffect(() => {
-      axios.get("https://swapi.py4e.com/api/people/")
-      
-      .then(res => setStarWars(res.data.results))
-      
-      .catch(err => console.log(err, "error"))
 
-    })
-  }
+const App = () => {
+
+  const [starWars, setStarWars] = useState([])
+
+
+  useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/")
+    .then(response => setStarWars(response.data.results))
+    .catch(err => console.log(err, "error received"))
+  }, []);
+
+
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+
+      <div className="starChar" >
+        {starWars.map((item, idx) => (
+          <Character key ={idx} cards={item} />
+          ))}
+      </div>
+          
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,39 @@
 // Write your Character component here
+
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import styled from 'styled-components'
+
+
+const CardBackground = styled.div`
+    background-color:white;
+    margin:2%;
+    padding: 1%;
+    border-radius:30px;
+    `;
+
+
+const Cards = props => {
+console.log(props.cards);
+    return (
+
+<div className="cards">
+    <div>Name: {props.cards.name}</div>
+    <p>Height: {props.cards.height}</p>
+    <p>Weight: {props.cards.mass}</p>
+    <p>Hair Color: {props.cards.hair_color}</p>
+    <p>Eye Color: {props.cards.eye_color}</p>
+    <p>Skin Color: {props.cards.skin_color}</p>
+    <p>Birth Year: {props.cards.birth_year}</p>
+    <p>Gender: {props.cards.gender}</p>
+    <p>Homeworld: {props.cards.homeworld}</p>
+</div>
+
+
+    )
+}
+
+
+
+
+export default Cards;
